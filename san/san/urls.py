8107df04c850fc2362e8fa8c8aa5fcf09d17c5cc
sanjaigant <sanjaiganth07@gmail.com>
2025-05-02 08:36:42 +0530

    Success

---
+++ san/san/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin 
+from django.urls import path 
+from mathapp import views 
+urlpatterns = [ 
+    path('admin/', admin.site.urls), 
+    path('powerir/',views.power,name="powerir"),
+    path('',views.power,name="powerir")
+]
